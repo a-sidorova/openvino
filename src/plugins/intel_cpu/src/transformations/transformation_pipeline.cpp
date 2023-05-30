cb122137204e69f61ba3648abb1267dced162928
Alexandra Sidorova <alexandra.sidorova@intel.com>
2023-05-30 17:15:53 +0400

    Disabled snippets

--- src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
+++ src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
@@ -612,6 +612,7 @@ void Transformations::PostLpt() {
 }
 
 void Transformations::MainSnippets(void) {
+    return;
     if (snippetsMode == Config::SnippetsMode::Disable ||
         !dnnl::impl::cpu::x64::mayiuse(dnnl::impl::cpu::x64::avx2)) // snippets are implemented only for relevant platforms (avx2+ extensions)
         return;
